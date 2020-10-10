b12f2ef27f7f84d61a0e7f26de93fd6cb68d88de
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-10-10 16:38:36 -0400

    added templates

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return HttpResponse('<h1>Blog About</h1>')
+
+posts = [
+    {
+        'author': 'elyse',
+        'title': 'how to airflow',
+        'content': 'here is how to do it',
+        'date_posted': 'august 1, 2020'
+    },
+{
+        'author': 'elyse',
+        'title': 'how to sql',
+        'content': 'here is how to do it',
+        'date_posted': 'september 1, 2020'
+    }
+]
